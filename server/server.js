59d728497aa14fdafd1a02eb0e0db724e856258b
VoDinhNat <vonhat570@gmail.com>
2026-01-12 23:49:32 +0700

    user registration, login, and logout

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
+const authRouter = require("./routes/auth/auth-routes");
+
 dotenv.config();
 
 const app = express();
@@ -20,6 +22,8 @@ app.use(cors({
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
