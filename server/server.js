449d64092b9938384aa0ba259d995ff60b4926ce
VoDinhNat <vonhat570@gmail.com>
2026-01-18 15:21:34 +0700

    Add product review and search featureses

--- server/server.js
+++ server/server.js
@@ -12,6 +12,8 @@ const shopProductsRouter = require("./routes/shop/products-routes");
 const shopCartRouter = require("./routes/shop/cart-routes");
 const shopAddressRouter = require("./routes/shop/address-routes");
 const shopOrderRouter = require("./routes/shop/order-routes");
+const shopSearchRouter = require("./routes/shop/search-routes");
+const shopReviewRouter = require("./routes/shop/review-routes");
 
 dotenv.config();
 
@@ -39,6 +41,9 @@ app.use("/api/shop/products", shopProductsRouter);
 app.use("/api/shop/cart", shopCartRouter);
 app.use("/api/shop/address", shopAddressRouter);
 app.use("/api/shop/order", shopOrderRouter);
+app.use("/api/shop/search", shopSearchRouter);
+app.use("/api/shop/review", shopReviewRouter);
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
