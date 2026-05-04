107cf63a2ea0d0a5955d04a43e08aaed2f687df6
Dinhnhat057 <vonhat570@gmail.com>
2026-05-04 15:10:12 +0700

    Update order-controller.js

--- server/controllers/shop/order-controller.js
+++ server/controllers/shop/order-controller.js
@@ -26,8 +26,8 @@ const createOrder = async (req, res) => {
                 payment_method: "paypal",
             },
             redirect_urls: {
-                return_url: "https://final-3fhba0wg2-vonhat570-4496s-projects.vercel.app/shop/paypal-return",
-                cancel_url: "https://final-3fhba0wg2-vonhat570-4496s-projects.vercel.app/shop/paypal-cancel",
+                return_url: "http://localhost:5173/shop/paypal-return",
+                cancel_url: "http://localhost:5173/shop/paypal-cancel",
             },
             transactions: [
                 {
